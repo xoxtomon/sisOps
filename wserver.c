e8db541b74968fcc10d59061d13ca058b3e1b866
xoxtomon <44625096+xoxtomon@users.noreply.github.com>
2021-09-18 19:01:35 -0500

    update intercommunication done

--- wserver.c
+++ wserver.c
@@ -4,15 +4,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <uuid/uuid.h>
 #include "request.h"
 #include "io_helper.h"
 
-struct estMensaje
+typedef struct estMensaje
 {
 	long int msg_type;
-	char *incial;
-	char *final;	
-};
+	char incial[50];
+	char final[50];
+	char uuid[50];	
+} estMensaje;
+
+typedef struct estMensaje2{
+	long int msg_type;
+	time_t tiempo1;
+	time_t tiempo2;
+}estMensaje2;
 
 char default_root[] = ".";
 
@@ -20,9 +28,7 @@ char default_root[] = ".";
 // ./wserver [-d <basedir>] [-p <portnum>] 
 // 
 int main(int argc, char *argv[]) {
-    //inicializar my estructura
-    struct estMensaje mensaje; 
-    int c, msg_id;
+	int c;
     char *root_dir = default_root;
     int port = 10000;
     
@@ -39,24 +45,51 @@ int main(int argc, char *argv[]) {
 	    exit(1);
 	}
 
+	//variables para comonicacion
+	int msg_id = 0;
+	estMensaje mensaje;
+	estMensaje2 mensaje2;
+
+	msg_id = msgget((key_t *)123, 0666 | IPC_CREAT);
+	long int msgtype;
+
     // run out of this directory
     chdir_or_die(root_dir);
 
     // now, get to work
-
     int listen_fd = open_listen_fd_or_die(port);
-
-    //crear la cola de mensajes
-    //lA LLAVE ES ´12345´ PERMISOS DE ESCRIBIR Y LEER
-    msg_id = msgget((key_t)12345,0666|IPC_CREAT);
-    if(msg_id ==-1){
-    	printf("La cola no pudo ser creada con exito.\n");
-    }
+    
     
     //Fork que se levanta con el servidor
     int init_fork = fork_or_die();
-    if (init_fork > 0){
-    	//listener parent >0
+    if (init_fork == 0){
+    	//listener parent
+    	//reader
+    	printf("listening\n");
+    	while(1){
+
+    		int aux = msgrcv(msg_id, (void *)&mensaje2, sizeof(estMensaje2), msgtype, 0);
+    		if(-1 == aux){
+    			printf("msgrcv fail\n");
+    			exit(-1);
+    		}
+
+    		if(mensaje2.msg_type == -1){
+    			break;
+    		}
+
+    		uuid_t out;
+    		uuid_generate_random(&out);
+
+    		printf("recibi: %s %s %s\n",ctime(&mensaje2.tiempo1), ctime(&mensaje2.tiempo2),out);
+    	}
+
+    	if(msgctl(msg_id,IPC_RMID,0)==1){
+    		printf("MSGCTL(IPC_RMID) failed\n");
+    	}
+    }
+    else if(init_fork > 0){
+    	//hijo hace requests
     	while (1) {
 			struct sockaddr_in client_addr;
 			int client_len = sizeof(client_addr);
@@ -65,46 +98,41 @@ int main(int argc, char *argv[]) {
 			//Fork que maneja los requests
 			int request_fork = fork_or_die();
 			if (request_fork == 0){
-				time_t initialTime;
-				char *initialTimeString;
-				initialTime = time(NULL);
-				initialTimeString = ctime(&initialTime);
 
-				time_t finalTime;
-				char *finalTimeString;
+				//Toma de tiempo cuando se inicial la peticion
+				time_t initialTime;
+				initialTime = time(NULL);
+
 				//printf("[son] pid %lu from [parent] pid %lu\n", getpid(), getppid());
-		
+				
 				request_handle(conn_fd);
 				close_or_die(conn_fd);
 
+
+				//Toma de timepo cuando se finaliza la peticion
+				time_t finalTime;
 				finalTime = time(NULL);
-				finalTimeString = ctime(&finalTime);
 
-				//sender
-				mensaje.msg_type = 1;
-				mensaje.incial = initialTimeString;
-				mensaje.final = finalTimeString;
-				//printf("%d\n",mensaje.msg_type);
-				printf("%s %s\n",mensaje.incial, mensaje.final );
+				mensaje2.msg_type = 1;
+				mensaje2.tiempo1 = initialTime;
+				mensaje2.tiempo2 = finalTime;
 
-				//enviar el mensaje
-				msgsnd(msg_id, &mensaje, sizeof(mensaje),0);
+				//mandar info
+				int tmp = msgsnd(msg_id, (void *)&mensaje2, sizeof(estMensaje2), 0);
+				if(-1 == tmp){
+					printf("msgsnd() failed\n");
+				}
+
+				if(mensaje2.msg_type == -1){
+					break;
+				}
+				
+				printf("le mande: %s %s \n",ctime(&mensaje2.tiempo1), ctime(&mensaje2.tiempo2));
+
 				exit(0);
 			}
     	}
     }
-    else if(init_fork == 0){
-    	//listener child == 0
-    	//reader
-    	printf("listening\n");
 
-    	while(1){
-    		msgrcv(msg_id, &mensaje, sizeof(mensaje), 1, 0);
-
-    		printf("desde listener: %s %s\n",mensaje.incial, mensaje.final);
-
-    		msgctl(msg_id, IPC_RMID, NULL);
-    	}	
-    }
     return 0;
 }
